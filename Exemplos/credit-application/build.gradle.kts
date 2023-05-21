05d0e55caf73170ba15009490fff2f0fdf4004bd
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2023-05-21 20:56:46 -0300

    Document with Swagger-UI

--- Exemplos/credit-application/build.gradle.kts
+++ Exemplos/credit-application/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- Exemplos/credit-application/src/main/kotlin/edu/victor/creditapplication/Controller/CustomerController.kt
+++ Exemplos/credit-application/src/main/kotlin/edu/victor/creditapplication/Controller/CustomerController.kt
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("/api/customer")
 class CustomerController(
         private val customerService: CustomerService
 ) {
